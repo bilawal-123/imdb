a51f30ae7ac83c5dab37e1556fa71cbf31665c0a
bilawal-123 <bilawal.officials@gmail.com>
2023-09-26 18:26:03 +0500

    Custom error code to fetch data. Error.js

--- src/app/page.js
+++ src/app/page.js
@@ -11,6 +11,9 @@ export default async function Home({ searchParams }) {
     }?api_key=${API_KEY}&language=en-US&page=1`,
     { next: { revalidate: 10000 } }
   );
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
   const data = await res.json();
   const results = data.results;
   console.log(results);
